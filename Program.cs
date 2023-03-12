898ea254352dcf46be3eeb28409e6879b1504d52
Ana Shevchenko <shevchenko_a@list.ru>
2023-03-12 19:14:23 +0300

    Ввод исходного массива

--- Program.cs
+++ Program.cs
@@ -2,3 +2,37 @@
 // формирует массив из строк, длина которых меньше или равна 3 символам.
 
 
+void FillArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = InputString($"Введите строку {i+1} массива: ");
+    }
+}
+
+int InputInt(string message)
+{
+    Console.Write($"{message}: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+string InputString(string message)
+{
+    Console.Write($"{message}: ");
+    return Convert.ToString(Console.ReadLine());
+}
+
+
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}\t");
+    }
+}
+
+int arraylength = InputInt("Введите количество элементов в массиве");
+
+string[] array = new string[arraylength];
+FillArray(array);
+PrintArray(array);
